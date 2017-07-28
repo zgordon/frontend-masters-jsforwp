c33f527b16ac4b8943884c4669c1684b109b7b1c
Jacob Arriola <jacob@zeek.com>
2017-07-27 21:02:56 -0700

    Use vue shorthands for directives

--- vueapp/custom.php
+++ vueapp/custom.php
@@ -21,14 +21,14 @@
 
             <ul v-if="!isSingle">
                 <li v-for="post in posts" :key="post.id">
-                    <a v-bind:href="'#/'+post.slug" v-on:click="showPost( post )">
+                    <a :href="'#/'+post.slug" @click="showPost(post)">
                         {{ post.title.rendered }}
                     </a>
                 </li>
             </ul>
 
-                <p><a href="#/" v-on:click="showPosts"><< Back to Posts</a></p>
             <div v-else>
+                <p><a href="#/" @click="showPosts"><< Back to Posts</a></p>
                 <h2 v-text="post.title.rendered"></h2>
                 <div v-html="post.content.rendered"></div>
             </div>
